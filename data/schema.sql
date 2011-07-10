012f2240b48bc98bc5dc76328bba8d89a5d3700e
Dustin Cloues <dustin@clou.es>
2011-07-10 15:18:38 -0400

    track last seen times

--- data/schema.sql
+++ data/schema.sql
@@ -1,7 +1,19 @@
+DROP TABLE IF EXISTS chatter;
 CREATE TABLE chatter (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nick CHAR(9),
     channel VARCHAR(255) NOT NULL,
     chat_date DATETIME NOT NULL,
     chat VARCHAR(512) NOT NULL
+) engine=XtraDB;
+
+CREATE INDEX chatter_channel ON chatter (channel);
+CREATE INDEX chatter_chat_date ON chatter (chat_date);
+
+DROP TABLE IF EXISTS last_seen;
+CREATE TABLE last_seen (
+    nick CHAR(9) NOT NULL,
+    channel VARCHAR(255) NOT NULL,
+    last_seen DATETIME NOT NULL,
+    PRIMARY KEY (nick, channel)
 ) engine=XtraDB;
--- modules/history.py
+++ modules/history.py
@@ -2,6 +2,8 @@ import MySQLdb
 from threading import Thread
 
 class HistoryLoader(Thread):
+	since = None
+
 	def __init__(self, ctx, channel):
 		Thread.__init__(self)
 		self.ctx = ctx
@@ -10,8 +12,17 @@ class HistoryLoader(Thread):
 	def run(self):
 		c = MySQLdb.connect(user="root", db="chatter")
 		cursor = c.cursor()
+		
+		query = 'SELECT chat, nick, chat_date FROM chatter WHERE channel=%s AND chat_date > '
 
-		cursor.execute('SELECT chat, nick, chat_date FROM chatter WHERE channel=%s AND chat_date > now() - INTERVAL 1 DAY', (self.channel,))
+		if self.since is not None:
+			query = query + '%s'
+			args = (self.channel, self.since)
+		else:
+			query = query + 'now() - INTERVAL 1 DAY'
+			args = (self.channel)
+
+		cursor.execute(query, args)
 		while True:
 			row = cursor.fetchone()
 			if row is None:
@@ -21,17 +32,50 @@ class HistoryLoader(Thread):
 
 		c.close()
 
-def onLoad():
+class LastSeen(object):
+	connection = None
+
+	def __init__(self):
+		self.connection = MySQLdb.connect(user="root", db="chatter")
 	
+	def close(self):
+		if self.connection is not None:
+			self.connection.close()
+
+	def record(self, nick, chan):
+		cursor = self.connection.cursor()
+		cursor.execute("INSERT INTO last_seen (nick, channel, last_seen) VALUES (%s, %s, now()) ON DUPLICATE KEY UPDATE last_seen=now()", (nick, chan))
+
+		cursor.close()
+		self.connection.commit()
+
+	def get(self, nick, chan):
+		cursor = self.connection.cursor()
+		cursor.execute('SELECT last_seen from last_seen WHERE channel=%s and nick=%s', (chan, nick))
+		r = cursor.fetchone()
+		last = r[0] if r is not None else None
+		cursor.close()
+		return last
+
+def onLoad():
+	lastSeen = LastSeen()
+
 	@command("history", 1)
 	def onHistory(ctx, cmd, arg, *args):
-		ctx.reply("something: %s" % (arg))
 		loader = HistoryLoader(ctx, arg)
+		loader.start()
+
+	@command("unseen", 1)
+	def onUnseenHistory(ctx, cmd, arg, *args):
+		loader = HistoryLoader(ctx, arg)
+		loader.since = lastSeen.get(ctx.who.nick, arg)
 		loader.start()
 	
 	@hook("message")
 	def onMessage(ctx, msg):
-		print("got a message: %s %s %s" % (ctx.chan, ctx.who.nick, msg))
+		if ctx.chan[0] is not '#':
+			return
+
 		c = MySQLdb.connect(user="root", db="chatter")
 		cursor = c.cursor()
 		cursor.execute('INSERT INTO chatter (channel, nick, chat_date, chat) VALUES (%s,%s,now(),%s)', (ctx.chan, ctx.who.nick, msg))
@@ -39,3 +83,10 @@ def onLoad():
 		c.commit()
 		c.close()
 
+	@hook("quit")
+	def onQuit(ctx):
+		lastSeen.record(ctx.who.nick, ctx.chan)
+	
+	@hook("part")
+	def onPart(ctx):
+		lastSeen.record(ctx.who.nick, ctx.chan)
